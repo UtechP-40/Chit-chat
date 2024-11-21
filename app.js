be825e0d4c3d0a8ba1a1b4c9283280cfb11ac82d
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-11-22 00:18:52 +0530

    Is_Online Feature Added

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ require('dotenv').config()
 // const mongoose = require('mongoose')
 const connectDb = require("./database/index.js")
 const userRoute = require("./routes/user.routes.js")
+const User = require("./models/user.model.js")
 const app = require('express')()
 app.use(require('express').static('public')) 
 app.set("view engine","ejs")
 app.set("views","./views")
+
+
+
 
 // const bodyParser = require("body-parser")
 // const upload = require("./middlewares/multer.middleware.js") 
@@ -28,10 +32,32 @@ app.use("/",userRoute)
 //     res.render("index")
 // })
 // const userRoute = require("./routes/user.routes.js")
+const io = require("socket.io")(http)
 
+const userNamespace=io.of('/user-namespace')
 
+userNamespace.on('connection',async (socket)=>{
+    console.log('user connected')
+   try {
+    
+    //  socket.on("sender_id",async (sender_id)=>{
+    //     id=sender_id
+        //  const updatedUser = await User.findOneAndUpdate({_id:socket.handshake.auth.token},{is_online:"1"})
+        const updatedUser = await User.findByIdAndUpdate({_id:socket.handshake.auth.token},{ $set:{is_online:"1"}})
+         console.log(updatedUser)
+    //  })
 
+    // console.log(socket)
 
+     socket.on("disconnect",async ()=>{
+         console.log("user Disconnected")
+         const updatedUser = await User.findByIdAndUpdate({_id:socket.handshake.auth.token},{ $set:{is_online:"0"}})
+        //  console.log(updatedUser)
+     })
+   } catch (error) {
+    console.log(error)
+   }
+})
 connectDb().then(()=>{
     http.listen(3000,()=>{
         console.log(`Server is running on http://localhost:3000`)
